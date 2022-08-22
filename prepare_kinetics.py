6b745c84ba7e93043c86a928c01849ab6cb9cd59
yongsun <ysyoon@xinapse.ai>
2022-08-22 23:34:12 +0000

    Add kinetics

---
+++ prepare_kinetics.py
@@ -0,0 +1,17 @@
+from torchvision.datasets import Kinetics
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--num_classes', type=str, default='700')
+parser.add_argument('--output_dir', type=str, default='kinetics')
+parser.add_argument('--num_download_workers', type=int, default=10)
+args = parser.parse_args()
+
+
+def main(args):
+    root = f'{args.output_dir}-{args.num_classes}'
+    Kinetics(root=root, frames_per_clip=32, num_classes=args.num_classes, download=True, num_download_workers=args.num_download_workers)
+    pass
+
+if __name__ == '__main__':
+    main(args)
